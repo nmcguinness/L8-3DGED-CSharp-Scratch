efc66e0776a3382de549dbdca805ee66e2c89a95
Niall McGuinness <niall.mcguinness@dkit.ie>
2026-09-22 12:37:48 +0100

    Added interface demo classes

---
+++ L8-3DGED-CSharp-Scratch/Explosion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace L8_3DGED_CSharp_Scratch
+{
+    /// <summary>
+    /// Applies damage to a set of targets at once.
+    /// </summary>
+    public class Explosion
+    {
+        private readonly List<IDamageable> _targets = new List<IDamageable>();
+
+        /// <summary>
+        /// Registers a target to be damaged when this explosion detonates.
+        /// </summary>
+        /// <param name="target">The target to register.</param>
+        public void Add(IDamageable target)
+        {
+            _targets.Add(target);
+        }
+
+        /// <summary>
+        /// Damages every registered target.
+        /// </summary>
+        /// <param name="damage">Points of damage to apply to each target.</param>
+        public void Detonate(int damage)
+        {
+            foreach (IDamageable target in _targets)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ L8-3DGED-CSharp-Scratch/IDamageable.cs
@@ -0,0 +1,48 @@
+﻿namespace L8_3DGED_CSharp_Scratch
+{
+    /// <summary>
+    /// A target that can receive damage.
+    /// </summary>
+    public interface IDamageable
+    {
+        /// <summary>
+        /// Applies damage to this target.
+        /// </summary>
+        /// <param name="amount">Points of damage to apply.</param>
+        void TakeDamage(int amount);
+    }
+
+    public class Barrel : IDamageable
+    {
+        private int _integrity = 20;
+
+        /// <inheritdoc />
+        public void TakeDamage(int amount)
+        {
+            _integrity -= amount;
+
+            if (_integrity <= 0)
+            {
+                Explode();
+            }
+        }
+
+        private void Explode()
+        {
+            // Spawn effect, remove from the scene.
+        }
+    }
+
+    public class Enemy : IDamageable
+    {
+        private int _health = 100;
+        private bool _isAlerted;
+
+        /// <inheritdoc />
+        public void TakeDamage(int amount)
+        {
+            _health -= amount;
+            _isAlerted = true;
+        }
+    }
+}
